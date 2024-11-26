b02a0e7533a8f123b167f2d0492947a92f2a9a89
Мацнев Владимир Дмитриевич <vdmatsnev@edu.hse.ru>
2024-11-26 20:08:08 +0300

    refactoring part1

---
+++ Sources/YAEngine.h
@@ -0,0 +1,21 @@
+//
+//  YAEngine.h
+//  YAEngine
+//
+
+#if canImport(UIKit)
+#import <UIKit/UIKit.h>
+#elseif canImport(Cocoa)
+#import <Cocoa/Cocoa.h>
+#endif
+
+
+//! Project version number for Glide.
+FOUNDATION_EXPORT double GlideEngineVersionNumber;
+
+//! Project version string for Glide.
+FOUNDATION_EXPORT const unsigned char GlideEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GlideEngine/PublicHeader.h>
+
+
