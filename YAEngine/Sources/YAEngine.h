05b5732b0e7b2c53e84e1d8a1abe37afd64c27a5
Мацнев Владимир Дмитриевич <vdmatsnev@edu.hse.ru>
2024-12-09 12:42:45 +0300

    fuck

--- YAEngine/Sources/YAEngine.h
+++ YAEngine/Sources/YAEngine.h
@@ -10,10 +10,10 @@
 #endif
 
 
-//! Project version number for Glide.
+//! Project version number for YAEngine.
 FOUNDATION_EXPORT double YAEngineVersionNumber;
 
-//! Project version string for Glide.
+//! Project version string for YAEngine.
 FOUNDATION_EXPORT const unsigned char YAEngineVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GlideEngine/PublicHeader.h>
